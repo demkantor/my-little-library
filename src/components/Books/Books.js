4d8f6e2cddf824681a8782bc4c12cc18b35bf580
demkantor <demkantor@gmail.com>
2020-06-23 16:48:53 -0500

    set up table logic for books

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -18,14 +18,65 @@ const Books = () => {
                     </span> 
                     Delete
                 </button>
+            </div>
             <div className="content-wrapper">
                 <div className="books-search">
-
+                    <form className="books-form">
+                        <input type="text" placeholder="Title" />
+                        <input type="text" placeholder="Author" />
+                        <select name="status">
+                            <option value="">--</option>
+                            <option value="available">Available</option>
+                            <option value="unavailable">Unavailable</option>
+                        </select> 
+                    </form>
+                    <div className="search-btn-container">
+                        <button className="card-btn">
+                            <span className="material-icons btn-icon">
+                                search
+                            </span> 
+                            Search
+                        </button>
+                        <button className="card-btn white">
+                            <span className="material-icons btn-icon">
+                                refresh
+                            </span> 
+                            Reset
+                        </button>
+                    </div>
                 </div>
                 <div className="books-table">
-                    
+                    <table>
+                        <thead>
+                            <tr>
+                                <th><input type="checkbox"/></th>
+                                <th></th>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td><img src="/images/blink.png" alt="blink" className="" width="80px"/></td>
+                                <td>Title</td>
+                                <td>Author</td>
+                                <td>Status</td>
+                                <td>view edit delete</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td><img src="/images/god-delusion.png" alt="The God Delusion" className="" width="80px"/></td>
+                                <td>Title</td>
+                                <td>Author</td>
+                                <td>Status</td>
+                                <td>view edit delete</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-            </div>
             </div>
         </div>
     )
