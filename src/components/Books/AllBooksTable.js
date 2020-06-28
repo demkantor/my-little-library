ddfc6adf61702caab7d404a1dfe079514b1059cf
demkantor <demkantor@gmail.com>
2020-06-28 15:08:19 -0500

    moved book table to it's own component

---
+++ src/components/Books/AllBooksTable.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import './Books.css';
+
+const AllBooksTable = ({ handleMainCheck, handleSingleCheck, deleteSelected }) => {
+    const dispatch = useDispatch();
+    const allBooks = useSelector(state => state.book.allBooks);
+
+    useEffect(() => {
+        dispatch({type: 'GET_ALL_BOOKS'});
+    }, [dispatch]);
+
+    return (
+        <div className="books-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" onChange={handleMainCheck}/></th>
+                        <th></th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Status</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allBooks ? 
+                        allBooks.map((book) => (
+                            <tr key={book._id}>
+                                <td>
+                                    <input
+                                        type="checkbox" 
+                                        name="singleCheck"
+                                        onChange={()=>handleSingleCheck(book._id)}/>
+                                </td>
+                                <td><img src={book.image} alt={book.title} className="" width="80px" height="80px"/></td>
+                                <td>{book.title}</td>
+                                <td>{book.author}</td>
+                                <td>
+                                    {book.status 
+                                    ?
+                                    <button className="card-btn green-btn">
+                                        Available
+                                    </button>
+                                    :
+                                    <button className="card-btn red-btn">
+                                        Unavailable
+                                    </button>
+                                    }
+                                </td>
+                                <td>
+                                    <Link to="/books/book._id">
+                                        <button className="table-btn">
+                                            view
+                                        </button>
+                                    </Link>
+                                    <Link to="/books/book._id">
+                                        <button className="table-btn">
+                                            edit
+                                        </button>
+                                    </Link>
+                                    <button 
+                                        className="table-btn"
+                                        onClick={()=>deleteSelected(book._id)}>
+                                            delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    : 'loading...'
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default AllBooksTable;
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,21 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import AllBooksTable from './AllBooksTable';
 import './Books.css';
 
 const Books = () => {
-    const dispatch = useDispatch();
-    const allBooks = useSelector(state => state.book.allBooks);
-
+    
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [status, setStatus] = useState('');
     const [errors, setErrors] = useState('');
     const [checked, setChecked] = useState([]);
-
-    useEffect(() => {
-        dispatch({type: 'GET_ALL_BOOKS'});
-    }, [dispatch]);
 
     const clearForm = () => {
         setTitle('');
@@ -28,25 +22,30 @@ const Books = () => {
             console.log('deleteing item(s) by checkmark...');
         } else {
             console.log('deleteing this...', id);
-        }
-        
+        };
     };
 
     const handleMainCheck = () => {
         // console.log(event.target.attributes)
         const childChecks = document.getElementsByName('singleCheck');
-        childChecks.forEach((child) => child.toggleAttribute("checked"))
+        childChecks.forEach((child) => child.toggleAttribute("checked"));
     };
 
     const handleSingleCheck = (id) => {
         console.log('check', id);
-        setChecked([...checked, id]);
+        if(checked.includes(id)) {
+            let newList = checked.filter(box => box.indexOf(id) !== -1);
+            setChecked(newList);
+        }else {
+            setChecked([...checked, id]);
+        };
+        console.log(checked);
     };
 
     const search = () => {
         if(title === "" && author === "" && status === ""){
             setErrors('Must have criteria in at least one field!');
-        }
+        };
         console.log('searching db...', title, author, status);
     };
 
@@ -109,72 +108,15 @@ const Books = () => {
                         <h3>
                             {errors}
                         </h3>
+                    </div>
                 </div>
-                </div>
-                <div className="books-table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th><input type="checkbox" onChange={handleMainCheck}/></th>
-                                <th></th>
-                                <th>Title</th>
-                                <th>Author</th>
-                                <th>Status</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {allBooks ? 
-                                allBooks.map((book) => (
-                                    <tr key={book._id}>
-                                        <td>
-                                            <input
-                                                type="checkbox" 
-                                                name="singleCheck"
-                                                onChange={()=>handleSingleCheck(book._id)}/>
-                                        </td>
-                                        <td><img src={book.image} alt={book.title} className="" width="80px" height="80px"/></td>
-                                        <td>{book.title}</td>
-                                        <td>{book.author}</td>
-                                        <td>
-                                            {book.status 
-                                            ?
-                                            <button className="card-btn green-btn">
-                                                Available
-                                            </button>
-                                            :
-                                            <button className="card-btn red-btn">
-                                                Unavailable
-                                            </button>
-                                            }
-                                        </td>
-                                        <td>
-                                            <Link to="/books/book._id">
-                                                <button className="table-btn">
-                                                    view
-                                                </button>
-                                            </Link>
-                                            <Link to="/books/book._id">
-                                                <button className="table-btn">
-                                                    edit
-                                                </button>
-                                            </Link>
-                                            <button 
-                                                className="table-btn"
-                                                onClick={()=>deleteSelected(book._id)}>
-                                                    delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                            : 'loading...'
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                <AllBooksTable 
+                    handleMainCheck={handleMainCheck}
+                    handleSingleCheck={handleSingleCheck}
+                    deleteSelected={deleteSelected}/>
             </div>
         </div>
-    )
+    );
 };
 
 export default Books;
